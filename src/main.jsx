36f8f0b1f3df34ab50dce1d42fa5676dbd5b0290
Makhkam Khalilloev <99492074+x-mahkam@users.noreply.github.com>
2026-06-30 17:28:59 +0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-// build: v37
+// build: v40
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AppProvider } from './context/AppContext.jsx'
+import ErrorBoundary from './components/common/ErrorBoundary.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <ErrorBoundary>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 )
