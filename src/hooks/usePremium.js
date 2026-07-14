6ac41a9bf5d8745c3760cac110f5de1e941d3212
Makhkam Khalilloev <99492074+x-mahkam@users.noreply.github.com>
2026-07-14 11:56:44 +0500

    Delete src/hooks directory

--- src/hooks/usePremium.js
+++
@@ -1,6 +0,0 @@
-import { useApp } from "../context/AppContext.jsx";
-
-export function usePremium() {
-  const { showPremModal, setShowPremModal, activatePremium } = useApp();
-  return { showPremModal, setShowPremModal, activatePremium };
-}
